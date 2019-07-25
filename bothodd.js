b7849db3a585eea4ccb5d7288850b6358852bdf0
revanth96 <revanthsm96@gmail.com>
2019-07-25 11:41:03 +0530

    Create bothodd.js

---
+++ bothodd.js
@@ -0,0 +1,13 @@
+function arebothodd(a,b)
+{
+    if((a%2 !=0) && (b%2 !=0))
+    {
+        console.log('true');
+    }
+    else
+    {
+        console.log('false');
+    }
+}
+var num1 = 25, num2 = 26;
+arebothodd(num1, num2);
